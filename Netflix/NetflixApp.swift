8da4a4f1de955218f73ce06371cd4fafd2d284f1
Devesh Tyagi <tyagi77@outlook.com>
2021-04-03 22:14:35 +0530

    Initial Commit

---
+++ Netflix/NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixApp.swift
+//  Netflix
+//
+//  Created by Devesh Tyagi on 03/04/21.
+//
+
+import SwiftUI
+
+@main
+struct NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
